3fe19e2cfae3c326f5580f5226ca55aeeba0c000
DavidCampsHz <davidcamposhz@gmail.com>
2022-03-07 20:16:19 +0000

    first footer layout

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+export const FooteR = () => {
+  return (
+    <>
+      <footer className="page-footer font-small blue pt-4">
+        <div class="container">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+            <div>
+              <h5>Ask to the Community</h5>
+            </div>
+            <div>
+              <h5>FAQ's</h5>
+            </div>
+            <div>
+              <h5>Contact Us</h5>
+            </div>
+            <div>
+              <h5>About Us</h5>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid text-end">© 2022 Copyright</div>
+        <div className="justify-content-center">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+          <i class="fa fa-facebook-official" aria-hidden="true"></i>
+        </div>
+      </footer>
+    </>
+  );
+};
