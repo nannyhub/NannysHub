5a5d713b1fa65f75215181055fb29c91fd47776e
MaiCodes-exe <maise.b.costa@gmail.com>
2022-03-08 16:25:22 +0000

    finished navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,17 +3,19 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-dark">
+    <nav className="navbar navbar-light bg-light" id="nav">
       <div className="container">
         <Link to="/">
-          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+          <span className="navbar-brand mb-0 h1">NannyHub</span>
         </Link>
-        <div className="ml-auto">
-          <Link to="/demo">
-            <button className="btn btn-primary">
-              Check the Context in action
-            </button>
-          </Link>
+        <div className="ml-auto"></div>
+        <div className="justify-content-end">
+          <button type="button" className="btn btn-link">
+            Login
+          </button>
+          <button type="button" className="btn btn-primary">
+            Sign Up
+          </button>
         </div>
       </div>
     </nav>
