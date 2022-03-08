e878f8f793f1d16781aa84ce4fb2e5b075f60ed1
AOP <aolivap@icloud.com>
2022-03-08 00:02:38 +0000

    added landing pages

---
+++ src/front/js/pages/landing.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Testimonials from "../misc/Testimonials";
+import Pricing from "../misc/Pricing";
+import Header from "../misc/Header";
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Pricing />
+      <Testimonials />
+    </React.Fragment>
+  );
+};
+
+export default Home;
+
+/*
+            <Pricing />
+            <Testimonials />
+*/
