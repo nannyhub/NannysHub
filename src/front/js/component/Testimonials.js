1244230d27bb21680f2756b9f63d2dd9d2d7e366
AOP <aolivap@icloud.com>
2022-03-08 00:17:30 +0000

    added testimonials

---
+++ src/front/js/component/Testimonials.js
@@ -0,0 +1,34 @@
+import React from "react";
+import user1 from "../../img/paty.jpg";
+import user2 from "../../img/nan.jpg";
+import "./styles/Testimonials.css";
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <section className="content-container">
+        <div className="textArea">
+          <h2>Testimonials</h2>
+          <p>What others say about us.</p>
+        </div>
+        <div className="cust-container">
+          <img src={user1} alt="Avatar" style={{ width: "90px" }} />
+          <p>
+            <span>John Doe.</span> Content Writer.
+          </p>
+          <p>Great attention to detail!</p>
+        </div>
+
+        <div className="cust-container">
+          <img src={user2} alt="Avatar" style={{ width: "90px" }} />
+          <p>
+            <span>Rebecca Flex.</span> Reporter
+          </p>
+          <p>Nice enviroment to run my personal projects.</p>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Home;
