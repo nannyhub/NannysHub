d1b27072bbfda8c4dbc409405825db3833f3ab4e
MaiCodes-exe <maise.b.costa@gmail.com>
2022-03-10 19:54:49 +0000

    updated navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light" id="nav">
-      <div className="container">
+    <nav className="navbar navbar-light">
+      <div className="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            id="mary"
+            src="https://behindthecurtaincincy.files.wordpress.com/2015/07/voi_mary-poppins-logo.jpg"
+            alt=""
+          ></img>
+        </a>
         <Link to="/">
-          <span className="navbar-brand mb-0 h1">NannyHub</span>
+          <span className="navbar-brand mb-0 h1" id="title">
+            NannyHub
+          </span>
         </Link>
         <div className="ml-auto"></div>
         <div className="justify-content-end">
-          <button type="button" className="btn btn-link">
+          <button type="button" className="btn" id="button1">
             Login
           </button>
-          <button type="button" className="btn btn-primary">
+          <button type="button" className="btn btn-light" id="button2">
             Sign Up
           </button>
         </div>
