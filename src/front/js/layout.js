84ee3c7724d1e298036c25fa921b81b36b2715e6
AOP <aolivap@icloud.com>
2022-03-13 23:06:35 +0000

    1st commit signup - err on style

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import Login from "./pages/login";
+import { Signup } from "./pages/signUp";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -31,6 +32,9 @@ const Layout = () => {
             </Route>
             <Route exact path="/login">
               <Login />
+            </Route>
+            <Route exact path="/signUp">
+              <Signup />
             </Route>
             <Route exact path="/single/:theid">
               <Single />
