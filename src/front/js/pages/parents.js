29411c090716fd51651caf316a777830867869c4
MaiCodes-exe <maise.b.costa@gmail.com>
2022-03-14 22:02:40 +0000

    fixed the problem with the form

--- src/front/js/pages/parents.js
+++ src/front/js/pages/parents.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../../styles/parents.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Parents = () => {
   return (
     <>
       <div className="container-fluid">
-        <div className="container">
+        <div className="container-fluid">
           <img
             src="https://previews.123rf.com/images/rosinka79/rosinka791704/rosinka79170400108/77286091-tambov-f%C3%A9d%C3%A9ration-de-russie-21-septembre-2016-portrait-de-la-famille-lego-figurines-d-un-p%C3%A8re-d-une-.jpg"
             id="profile"
@@ -13,74 +17,50 @@ const Parents = () => {
           <input id="bio"></input>
         </div>
         <div className="container fluid justify-content-center col-4" id="form">
-          <form>
-            <div className="row mb-3">
-              <label for="inputfirstname" className="col-sm-2 col-form-label">
-                First Name
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="firstname"
-                  className="form-control"
-                  id="firstname"
-                ></input>
-              </div>
-            </div>
-            <div className="row mb-3">
-              <label for="inputlastname" className="col-sm-2 col-form-label">
-                Last Name
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="lastname"
-                  className="form-control"
-                  id="lastname"
-                ></input>
-              </div>
-            </div>
-            <div className="row mb-3">
-              <label for="inputEmail3" className="col-sm-2 col-form-label">
-                Email
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="inputEmail3"
-                ></input>
-              </div>
-            </div>
-            <div className="row mb-3">
-              <label for="inputpassword" className="col-sm-2 col-form-label">
-                Password
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="password"
-                  className="form-control"
-                  id="password"
-                ></input>
-              </div>
-            </div>
-            <div className="row mb-3">
-              <label className="col-sm-2 col-form-label" id="children">
-                Number of Children
-              </label>
-              <select
-                className="form-select form-select-sm col-2"
-                aria-label=".form-select-sm example"
-              >
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-                <option value="more">More</option>
-              </select>
-            </div>
-          </form>
+          <Form>
+            <Form.Group className="mb-3" controlId="formFirst">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="first" placeholder="Enter your first name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formLast">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="last" placeholder="Enter your last name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formChild">
+              <Form.Label>Number of children</Form.Label>
+              <Dropdown>
+                <Dropdown.Toggle
+                  id="dropdown-button-example1"
+                  className="mb-3 col-12"
+                >
+                  1
+                </Dropdown.Toggle>
+                <Dropdown.Menu id="dropdown">
+                  <Dropdown.Item href="#/action-1" active>
+                    2
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">3</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">More</Dropdown.Item>
+                  <Dropdown.Divider />
+                </Dropdown.Menu>
+              </Dropdown>
+            </Form.Group>
+
+            <Button variant="primary" type="submit" id="button">
+              Submit
+            </Button>
+          </Form>
         </div>
-        <button type="button" className="btn btn-primary" id="button">
-          Save
-        </button>
       </div>
     </>
   );
