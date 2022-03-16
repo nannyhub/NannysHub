5cc7733dc5ef450cf7141fa636ef23cc8cb15e0e
AOP <aolivap@icloud.com>
2022-03-16 20:57:11 +0000

    3 tables db models

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,61 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, PrimaryKey, ForeignKey, Integer, String, Boolean, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy 
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Parent(db.Model):
+    # Here we define columns for the table planet.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, unique=True, primary_key=True)
+    first_name = Column(String(200), nullable=False)
+    last_name = Column(String(200), nullable=False)
+    number_of_children = Column(Integer(10), nullable=False)
+    parents = relationship('User.id', backref='User', lazy=True)
+    nanny = relationship('', backref='', lazy=True)
+
+    def __repr__(self):
+        return '<Parent %r>' % self.first_name
+
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "first_name": self.first_name,
+            "last_name": self.last_name
+        }
+
+
+
+class Favorites(db.Model):
+    id = Column(Integer, primary_key=True)
+    parent_id = Column(Integer, ForeignKey('Parent.id'))
+    nanny_id = Column(Integer, ForeignKey('Nanny.id'))
+
+    def __repr__(self):
+        return '<FavoriteCharacter %r>' % self.user_id
+
+    def serialize(self):
+        user = User.query.get(self.user_id)
+        return {
+            "parent_id": user.email,
+            "nanny_id": self.nanny_id,
         }
