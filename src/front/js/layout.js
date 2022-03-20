08472120086512198cc388f958c5d20fc6684070
vitortamberlini <vitor.tamberlini@outlook.com>
2022-03-20 15:59:04 -0300

    Fixed layout.js

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,59 +1,60 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./pages/landing";
-import { Single } from "./pages/single";
-import { FAQ } from "./component/faq";
+import {Home} from "./pages/landing";
+import {Single} from "./pages/single";
+import {FAQ} from "./component/faq";
 import Login from "./pages/login";
-import { Signup } from "./pages/signup";
+import {Signup} from "./pages/signup";
 import Contacts from "./pages/contacts";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import {Navbar} from "./component/navbar";
+import {Footer} from "./component/footer";
 
 //create your first component
 const Layout = () => {
-  //the basename is used when your project is published in a subdirectory and not in the root of the domain
-  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-  const basename = process.env.BASENAME || "";
+    //the basename is used when your project is published in a subdirectory and not in the root of the domain
+    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+    const basename = process.env.BASENAME || "";
 
-  return (
-    <div id="base-container">
-      <BrowserRouter basename={basename}>
-        <ScrollToTop>
-          <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/single/:theid">
-              <Single />
-            </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <Route exact path="/signup">
-              <Signup />
-            <Route exact path="/contacts">
-              <Contacts />
-            </Route>
-            <Route exact path="/single/:theid">
-              <Single />
-            </Route>
-            <Route exact path="/faq">
-              <FAQ />
-            </Route>
-            <Route>
-              <h1>Not found!</h1>
-            </Route>
-          </Switch>
-          <Footer />
-        </ScrollToTop>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div id="base-container">
+            <BrowserRouter basename={basename}>
+                <ScrollToTop>
+                    <Navbar/>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home/>
+                        </Route>
+                        <Route exact path="/single/:theid">
+                            <Single/>
+                        </Route>
+                        <Route exact path="/login">
+                            <Login/>
+                        </Route>
+                        <Route exact path="/signup">
+                            <Signup/>
+                        </Route>
+                        <Route exact path="/contacts">
+                            <Contacts/>
+                        </Route>
+                        <Route exact path="/single/:theid">
+                            <Single/>
+                        </Route>
+                        <Route exact path="/faq">
+                            <FAQ/>
+                        </Route>
+                        <Route>
+                            <h1>Not found!</h1>
+                        </Route>
+                    </Switch>
+                    <Footer />
+                </ScrollToTop>
+            </BrowserRouter>
+        </div>
+);
 };
 
 export default injectContext(Layout);
