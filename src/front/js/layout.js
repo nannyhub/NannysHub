6c457c8b75491feb7c6adcbdf6529158b2dbd6fc
vitortamberlini <vitor.tamberlini@outlook.com>
2022-03-20 16:01:13 -0300

    Fixed layout.js

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -14,43 +14,46 @@ import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
-  //the basename is used when your project is published in a subdirectory and not in the root of the domain
-  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-  const basename = process.env.BASENAME || "";
+    //the basename is used when your project is published in a subdirectory and not in the root of the domain
+    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+    const basename = process.env.BASENAME || "";
 
-  return (
-    <div id="base-container">
-      <BrowserRouter basename={basename}>
-        <ScrollToTop>
-          <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/single/:theid">
-              <Single />
-            </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <Route exact path="/signup">
-              <Signup />
-            </Route>
-            <Route exact path="/single/:theid">
-              <Single />
-            </Route>
-            <Route exact path="/faq">
-              <FAQ />
-            </Route>
-            <Route>
-              <h1>Not found!</h1>
-            </Route>
-          </Switch>
-          <Footer />
-        </ScrollToTop>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div id="base-container">
+            <BrowserRouter basename={basename}>
+                <ScrollToTop>
+                    <Navbar/>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home/>
+                        </Route>
+                        <Route exact path="/single/:theid">
+                            <Single/>
+                        </Route>
+                        <Route exact path="/login">
+                            <Login/>
+                        </Route>
+                        <Route exact path="/signup">
+                            <Signup/>
+                        </Route>
+                        <Route exact path="/contacts">
+                            <Contacts/>
+                        </Route>
+                        <Route exact path="/single/:theid">
+                            <Single/>
+                        </Route>
+                        <Route exact path="/faq">
+                            <FAQ/>
+                        </Route>
+                        <Route>
+                            <h1>Not found!</h1>
+                        </Route>
+                    </Switch>
+                    <Footer />
+                </ScrollToTop>
+            </BrowserRouter>
+        </div>
+);
 };
 
 export default injectContext(Layout);
