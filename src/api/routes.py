f6014d20a5d6edc4b80be82339a56051cb6ecdf8
AOP <aolivap@icloud.com>
2022-03-27 22:33:10 +0000

    login endpoint + store

--- src/api/routes.py
+++ src/api/routes.py
@@ -63,3 +63,41 @@ def create_user():
     db.session.commit()
     return "User has been created", 200
 
+    # Create a route to authenticate your users and return JWTs. The
+    # create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST", "GET"])
+def login():
+    body = request.get_json()
+    if body is None:
+        return jsonify({"error": "Body is empty or null"}), 400
+    email = body['email']
+    password = body['password']
+    # 1. read mail & pass
+    # email = request.json.get("email", None)
+    # password = request.json.get("password", None)
+    # password_hash = generate_password_hash(password) # removed #
+    user = User.lookup(email)
+    # 2 check error
+    if user and check_password_hash(user.password, password):
+    # 3. return token
+        access_token = create_access_token(identity=email)
+        return jsonify({'token' : access_token}), 200
+    else:
+        return {'error': 'user and pass not valid'}, 400
+
+@api.route('/refresh', methods=['POST'])
+def refresh():
+    print("refresh request")
+    old_token = request.get_data()
+    new_token = guard.refresh_jwt_token(old_token)
+    return {'access_token': new_token}, 200
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.filter.get(current_user_id)
+    return jsonify({"id": user.id, "email": user.email}), 200
