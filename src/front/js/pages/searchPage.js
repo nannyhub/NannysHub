ed297fa03e9ce99016b0ff091983a4a7e1f26089
MaiCodes-exe <maise.b.costa@gmail.com>
2022-04-04 19:25:29 +0000

    fixed the card display column issue

--- src/front/js/pages/searchPage.js
+++ src/front/js/pages/searchPage.js
@@ -46,87 +46,94 @@ export const SearchPage = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <Container className="bar">
-      <div className="d-flex flex-column justify-content-center align-items-center my-3 mx-5">
-        <InputGroup className="mb-3 w-50">
-          <Button variant="success" id="button-addon1">
-            <i className="fa-solid fa-magnifying-glass" />
-          </Button>
-          <FormControl
-            aria-label="Example text with button addon"
-            aria-describedby="basic-addon1"
-          />
-        </InputGroup>
-        <Row>
-          <Col>
-            <i className="fas fa-map-marker-alt"></i>
-          </Col>
-          <Col className="search">
-            <input
-              id="search-bar-city"
-              type="text"
-              placeholder="Filter by city"
-              onChange={(event) => {
-                setSearchByInfo({
-                  ...searchByInfo,
-                  location: event.target.value,
-                });
-              }}
-            />
-          </Col>
-          <Col>
-            <i className="fas fa-euro-sign"></i>
-          </Col>
-          <Col className="search">
-            <input
-              id="search-bar-amount"
-              type="text"
-              placeholder="Filter by € per hour"
-              onChange={(event) => {
-                setSearchByInfo({ ...searchByInfo, price: event.target.value });
-              }}
-            />
-          </Col>
-          <Col>
-            <i class="fa-solid fa-badge-check"></i>
-          </Col>
-          <Col className="search">
-            <input
-              id="search-bar-experience"
-              type="text"
-              placeholder="Filter by Experience"
-              onChange={(event) => {
-                setSearchByInfo({
-                  ...searchByInfo,
-                  experience: event.target.value,
-                });
-              }}
-            />
-          </Col>
-          <Col>
-            <Button
-              onClick={() => {
-                actions.getNannyApplyFilter(searchByInfo);
-              }}
-            >
-              Test
+    <>
+      <Container>
+        <div className="d-flex flex-column justify-content-center align-items-center my-3 mx-5">
+          <InputGroup className="mb-3 w-50">
+            <Button variant="success" id="button-addon1">
+              <i className="fa-solid fa-magnifying-glass" />
             </Button>
-          </Col>
-        </Row>
-        <div className="d-flex flex-column w-50 align-items-center">
-          <Wrapper apiKey={apiKey} render={render} />
-          <Slider
-            className="w-75"
-            defaultValue={50}
-            aria-label="Default"
-            valueLabelDisplay="auto"
-          />
+            <FormControl
+              aria-label="Example text with button addon"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+          <Row>
+            <Col>
+              <i className="fas fa-map-marker-alt"></i>
+            </Col>
+            <Col className="search">
+              <input
+                id="search-bar-city"
+                type="text"
+                placeholder="Filter by city"
+                onChange={(event) => {
+                  setSearchByInfo({
+                    ...searchByInfo,
+                    location: event.target.value,
+                  });
+                }}
+              />
+            </Col>
+            <Col>
+              <i className="fas fa-euro-sign"></i>
+            </Col>
+            <Col className="search">
+              <input
+                id="search-bar-amount"
+                type="text"
+                placeholder="Filter by € per hour"
+                onChange={(event) => {
+                  setSearchByInfo({
+                    ...searchByInfo,
+                    price: event.target.value,
+                  });
+                }}
+              />
+            </Col>
+            <Col>
+              <i class="fa-solid fa-badge-check"></i>
+            </Col>
+            <Col className="search">
+              <input
+                id="search-bar-experience"
+                type="text"
+                placeholder="Filter by Experience"
+                onChange={(event) => {
+                  setSearchByInfo({
+                    ...searchByInfo,
+                    experience: event.target.value,
+                  });
+                }}
+              />
+            </Col>
+            <Col>
+              <Button
+                onClick={() => {
+                  actions.getNannyApplyFilter(searchByInfo);
+                }}
+              >
+                Test
+              </Button>
+            </Col>
+          </Row>
+          <div className="d-flex flex-column w-50 align-items-center">
+            <Wrapper apiKey={apiKey} render={render} />
+            <Slider
+              className="w-75"
+              defaultValue={50}
+              aria-label="Default"
+              valueLabelDisplay="auto"
+            />
+          </div>
         </div>
+      </Container>
+      <div className="d-flex">
         {store.nannies.map((item) => {
           return (
-            <div className="d-flex flex-wrap w-50">
+            <div className="d-flex flex-wrap w-100">
               <Paper
-                className="d-flex align-items-center p-3 w-50"
+                className="d-flex align-items-center p-3 w-80"
                 elevation={0}
                 variant="outlined"
               >
@@ -145,6 +152,7 @@ export const SearchPage = () => {
           );
         })}
       </div>
-    </Container>
+      <br></br>
+    </>
   );
 };
